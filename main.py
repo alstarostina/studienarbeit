8324d68e63c7be1f29cd785cb89ea27b17867b18
wagnandr <wagnandr@in.tum.de>
2022-06-13 22:58:15 +0200

    new optimization

--- main.py
+++ main.py
@@ -2,93 +2,90 @@ import numpy as np
 import scipy
 from oct2py import octave
 
-x00 = np.array([
-	13., 
-	2., 
-	2., 
-	## Saeulen_f_{w,h,t}:
-	120., 
-	250., 
-	10.,
-	## T_{b,h,t,s}:
-	50.,
-	80.,
-	10.,
-	5.,
-	## I_haupt_{b,h,t,s}:
+x00_all = np.array([
+	# Platte_t,
+	# 29.3,
+	# 21.31157915, 
+	23.99104478,
+	# Waende_t,
+	# 21.9,
+	# 13.91926439,  
+	14.27326964,
+	# dg_surface_geom_t,
+	# 3.7,
+	# 1.23333333,
+	1.21834555,
+	# Beam_b,
 	100,
-	150,
+	# Beam_h,
+	70,
+	# Beam_t,
 	5,
+	# I_langtr_i_b,
+	50,
+	# I_langtr_i_h,
+	70,
+	# I_langtr_i_t,
+	3,
+	# I_langtr_i_s,
+	2,
+	# I_haupt_b,
+	100,
+	# I_haupt_h,
+	150,
+	# I_haupt_t,
+	5,
+	# I_haupt_s,
 	10,
-	## Saeulen_Seiten_{w,h,t}:
-	200,
+	# wichtig
+	# I_b,
+	50,
+	# I_h,
+	80,
+	# I_t,
+	10,
+	# I_s,
+	5,
+	# Saeulen_f_w,
+	120,
+	# Saeulen_f_h,
 	250,
-	10
+	# Saeulen_f_t
+	10,
 ])
+
+#x00 = x00_all[0:3]
+x00 = x00_all#[0:3]
 
 lower_bounds = x00/3
 upper_bounds = x00*3
-upper_bounds[1] = 20 
 
 bounds = scipy.optimize.Bounds(lower_bounds, upper_bounds, True)
 
 
 def extend_vector(vec):
-	# return [13., 2., 2.] + list(vec)
+	# return list(vec) + list(x00_all)[3:]
 	return list(vec)
-	'''
-	return [vec[0], 
-		vec[1], 
-		vec[2], 
-		120., 
-		250., 
-		10.,
-		## T_{b,h,t,s}:
-		50.,
-		80.,
-		10.,
-		5.,
-		## I_haupt_{b,h,t,s}:
-		100,
-		150,
-		5,
-		10,
-		## Saeulen_Seiten_{w,h,t}:
-		200,
-		250,
-		10
-	]
-	'''
 
 
 def print_vector(vec):
 	vec = extend_vector(vec)
-	print(f'Platte_t {vec[0]}')
-	print(f'Waende_t {vec[1]}')
-	print(f'dg_surface_geom_t {vec[2]}')
-	print(f'Saeulen_f_w {vec[3]}')
-	print(f'Saeulen_f_h {vec[4]}')
-	print(f'Saeulen_f_t {vec[5]}')
-	print(f'T_b {vec[6]}')
-	print(f'T_h {vec[7]}')
-	print(f'T_t {vec[8]}')
-	print(f'T_s {vec[9]}')
 	print(','.join([f'{v}' for v in list(vec)]))
 
 
 class OptimizationFunctional:
-	def __init__(self, penalty=1e-2, target_modal_energy=8.4) -> None:
+	def __init__(self, penalty=1e-1, target_modal_energy=8.4) -> None:
 		self.penalty = penalty 
 		self.target_modal_energy = target_modal_energy
 		self.msh_file = 'waggon.msh' 
 	
 	def __call__(self, vec, *args):
 		print(vec)
-		res = octave.train_analysis(self.msh_file, 1, *extend_vector(vec))
+		res = octave.train_analysis_v2(self.msh_file, 1, *extend_vector(vec))
 		mode, mass = res[0,0], res[0,1]
-		energy = (mode-self.target_modal_energy)**2 + self.penalty * mass**2
+		#energy = (mode-self.target_modal_energy)**2 + self.penalty * mass**2
 		#energy = - (mode)**2 + (mass-8.9)**2
-		#energy = - (mode)**2 + np.exp(100*(mass - 9)) 
+		energy = - (mode)**2 + 5e-1 * np.exp(100*(mass - 9)) 
 		#energy = - (mode)**2 + self.penalty * mass ** 2 
 		print(mode, mass, energy, vec)
 		return energy 
@@ -104,35 +101,7 @@ def saver(vec):
 	print('--<')
 
 
-x0 = np.array([
-	24.660, # 13., 
-	16.556, # 2., 
-	2.8503, # 2., 
-	## Saeulen_f_{w,h,t}:
-	120., 
-	250., 
-	10.,
-	## T_{b,h,t,s}:
-	50.,
-	80.,
-	10.,
-	5.,
-	## I_haupt_{b,h,t,s}:
-	100,
-	150,
-	5,
-	10,
-	## Saeulen_Seiten_{w,h,t}:
-	200,
-	250,
-	10
-])
-
-'''
-x0 = np.array([
-	14.743756944679314,4.420414172863627,1.8279984444801536,120.14542177730809,249.98855314314596,8.918429589739524,50.267894192867686,80.28236249331064,11.095596791847846,5.527072541931002,99.99820038096011,150.05626156496314,5.325610534242109,10.128540998765784,200.01434931621438,249.99692698246045,9
-])
-'''
+x0 = x00 
 
 res = scipy.optimize.minimize(
 	fun=to_optimize,
---
+++ train_analysis_v2.m
@@ -0,0 +1,335 @@
+function result = train_analysis_v2( 
+  waggon_msh_file,
+  nofmod,
+  Platte_t,
+  Waende_t,
+  dg_surface_geom_t,
+  Beam_b,
+  Beam_h,
+  Beam_t,
+  I_langtr_i_b,
+  I_langtr_i_h,
+  I_langtr_i_t,
+  I_langtr_i_s,
+  I_haupt_b,
+  I_haupt_h,
+  I_haupt_t,
+  I_haupt_s,
+  I_b,
+  I_h,
+  I_t,
+  I_s,
+  Saeulen_f_w,
+  Saeulen_f_h,
+  Saeulen_f_t
+) 
+
+fid = fopen("aufgabe2.res","wt");
+
+#material
+mat.type = "iso";
+mat.E = 70000;
+mat.ny = 0.34;
+mat.rho = 2.7e-9; %einheiten
+
+#Modell
+
+data.type = "solid";
+data.subtype = "3d";
+
+#BodenGruppe BG
+
+#Platte
+
+
+Platte.t = Platte_t; #mm
+
+data.BG_Platte = struct("type","elements",
+                        "name", "s4", 
+                        "geom", Platte, 
+                        "mat", mat);
+
+
+vector = [0,0,1];
+Beam.b = Beam_b;
+Beam.h = Beam_h;
+Beam.t = Beam_t;
+Kontakt_Langtr_rechts = [-Beam.b, -Beam.b];
+Kontakt_Langtr_links = [Beam.b, -Beam.b];
+
+geom_rechts = mfs_beamsection("box", "thin", Beam.b, Beam.h, Beam.t);
+geom_rechts.v = vector;
+geom_rechts.P = Kontakt_Langtr_rechts;
+
+geom_links = mfs_beamsection("box", "thin", Beam.b, Beam.h, Beam.t);
+geom_links.v = vector;
+geom_links.P = Kontakt_Langtr_links;
+
+#Langtr�ger rechts
+data.BG_Langtraeger_rechts = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_rechts, 
+                        "mat", mat);
+#Langtr�ger links
+data.BG_Langtraeger_links = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_links, 
+                        "mat", mat);
+                        
+#Langtr�ger innen
+I_langtr_i.b = I_langtr_i_b;
+I_langtr_i.h = I_langtr_i_h;
+I_langtr_i.t = I_langtr_i_t;
+I_langtr_i.s = I_langtr_i_s;
+Kreis.ri = 55;                      
+Kontakt_Langtr = [0, -0.5*(I_langtr_i.h)];
+geom_innen = geom_haupt = mfs_beamsection("I", I_langtr_i.b, I_langtr_i.h, I_langtr_i.t, I_langtr_i.s);
+geom_innen.v = vector;
+geom_innen.P = Kontakt_Langtr;
+data.BG_Langtraeger_innen = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_innen, 
+                        "mat", mat);
+
+#Quertr�ger
+#Hauptquertraeger
+I_haupt.b = I_haupt_b;
+I_haupt.h = I_haupt_h;
+I_haupt.t = I_haupt_t;
+I_haupt.s = I_haupt_s;
+P_haupt = [0, -(0.5*I_haupt.h)];
+
+geom_haupt = mfs_beamsection("I", I_haupt.b, I_haupt.h, I_haupt.t, I_haupt.s);
+geom_haupt.v = vector;
+geom_haupt.P = P_haupt;
+data.BG_Hauptquertraeger = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_haupt, 
+                        "mat", mat);
+
+I = struct("b", I_haupt.b*0.5, "h", I_haupt.t*0.5, ...
+           "t", I_haupt.t*0.5, "s", I_haupt.s*0.5);
+           
+geom =  mfs_beamsection("I", I.b, I.h, I.t, I.s);
+P_quer = [0, -(0.5*I.h)];
+geom.v = vector;  
+geom.P = P_quer; 
+data.BG_Quertraeger = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom, 
+                        "mat", mat);      
+                        
+                        
+                        
+data.BG_Quertraeger_AD = data.BG_Quertraeger;
+data.BG_Quertraeger_BC = data.BG_Quertraeger;
+
+                        
+                    
+
+#Seitengruppe SG
+
+Waende.t = Waende_t; #mm
+data.SG_Waende = struct("type","elements",
+                        "name", "s4", 
+                        "geom", Waende, 
+                        "mat", mat); 
+
+#Dach- und Fenstertr�ger
+vector = [0,0,1];
+I.b = I_b;
+I.h = I_h;
+I.t = I_t;
+I.s = I_s;
+P_links = [0.5*I.b, -0.5*I.h];
+P_rechts = [-0.5*I.b, -0.5*I.h];
+
+geom_links = mfs_beamsection("I", I.b, I.h, I.t, I.s);
+geom_links.v = vector;
+geom_links.P = P_links;
+geom_rechts = mfs_beamsection("I", I.b, I.h, I.t, I.s);
+geom_rechts.v = vector;
+geom_rechts.P = P_rechts;
+
+data.SG_Dachtraeger_links = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_links, 
+                        "mat", mat);
+data.SG_Dachtraeger_rechts = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_rechts, 
+                        "mat", mat);
+
+Beam_fenster.w = 40;
+Beam_fenster.h = 30;
+Beam_fenster.t = 3;
+geom_fenster_links = mfs_beamsection("box", "thin", Beam_fenster.w, Beam_fenster.h, Beam_fenster.t);
+geom_fenster_links.v = vector;
+geom_fenster_links.P = P_links;
+
+geom_fenster_rechts = mfs_beamsection("box", "thin", Beam_fenster.w, Beam_fenster.h, Beam_fenster.t);
+geom_fenster_rechts.v = vector;
+geom_fenster_rechts.P = P_links;
+                        
+data.SG_Fenstertraeger_links  = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_fenster_links, 
+                        "mat", mat);
+data.SG_Fenstertraeger_rechts = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_fenster_rechts, 
+                        "mat", mat);
+
+#S�ulen
+
+vector = [0,1,0];
+Saeulen_Seiten.w = 200;
+Saeulen_Seiten.h = 250;
+Saeulen_Seiten.t = 10;
+Punkt_links = [0.5*Saeulen_Seiten.w,0];
+Punkt_rechts = [-0.5*Saeulen_Seiten.w,0];
+
+geom_links = mfs_beamsection("box", "thin", Saeulen_Seiten.w, Saeulen_Seiten.h, Saeulen_Seiten.t);
+geom_links.v = vector;
+geom_links.P = Punkt_links;
+geom_rechts = mfs_beamsection("box", "thin", Saeulen_Seiten.w, Saeulen_Seiten.h, Saeulen_Seiten.t);
+geom_rechts.v = vector;
+geom_rechts.P = Punkt_rechts;
+
+Saeule_struct_links = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_links, 
+                        "mat", mat);
+Saeule_struct_rechts = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_rechts, 
+                        "mat", mat);                        
+
+data.SG_A_Saeule_links  = Saeule_struct_links;
+data.SG_B_Saeule_links = Saeule_struct_links;
+data.SG_C_Saeule_links = Saeule_struct_links;
+data.SG_D_Saeule_links = Saeule_struct_links;
+
+                        
+data.SG_A_Saeule_rechts = Saeule_struct_rechts;
+data.SG_B_Saeule_rechts = Saeule_struct_rechts;
+data.SG_C_Saeule_rechts = Saeule_struct_rechts;
+data.SG_D_Saeule_rechts = Saeule_struct_rechts;
+
+#Fensters�ulen
+
+vector = [0,1,0];
+Saeulen_f.w = Saeulen_f_w;
+Saeulen_f.h = Saeulen_f_h;
+Saeulen_f.t = Saeulen_f_t;
+Punkt_links_f = [0.5*Saeulen_f.w,0];
+Punkt_rechts_f = [-0.5*Saeulen_f.w,0];
+
+geom_links = mfs_beamsection("box", "thin", Saeulen_f.w, Saeulen_f.h, Saeulen_f.t);
+geom_links.v = vector;
+geom_links.P = Punkt_links_f;
+geom_rechts = mfs_beamsection("box", "thin", Saeulen_f.w, Saeulen_f.h, Saeulen_f.t);
+geom_rechts.v = vector;
+geom_rechts.P = Punkt_rechts_f;
+Saeule_links_f = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_links, 
+                        "mat", mat);
+Saeule_rechts_f = struct("type","elements",
+                        "name", "b2", 
+                        "geom", geom_rechts, 
+                        "mat", mat);     
+
+data.SG_Fenstersaeulen_links = Saeule_links_f;
+data.SG_Fenstersaeulen_rechts = Saeule_rechts_f;
+
+#Dachgruppe DG
+
+# Dachgruppe
+# ------------------------------------------------------------------------------
+
+# Fundamentaldachb�gen (Balken)
+
+dg_fdb_beam.type                =                   "elements";
+dg_fdb_beam.name                =                         "b2";
+dg_fdb_beam.geom                = mfs_beamsection("box", "thick",
+                                  80, 80, 20);
+dg_fdb_beam.geom.v              =                    [0, 0, 1];
+dg_fdb_beam.geom.P              =                   [0, -37.5];
+dg_fdb_beam.mat                 =                          mat;
+
+data.DG_Dachboegen_AD           =                  dg_fdb_beam;
+
+# Strukturdachb�gen (Balken)
+
+dg_sdb_beam.type                =                   "elements";
+dg_sdb_beam.name                =                         "b2";
+dg_sdb_beam.geom                = mfs_beamsection("I",
+                                  60, 60, 3, 5);
+dg_sdb_beam.geom.v              =                    [0, 0, 1];
+dg_sdb_beam.geom.P              =                     [0, -32];
+dg_sdb_beam.mat                 =                          mat;
+
+data.DG_Dachboegen_BC           =                  dg_sdb_beam;
+data.DG_Dachboegen              =                  dg_sdb_beam;
+
+# L�ngstr�ger (Balken)
+
+dg_lt_beam.type                 =                   "elements";
+dg_lt_beam.name                 =                         "b2";
+dg_lt_beam.geom                 = mfs_beamsection("I",
+                                  10, 15, 2, 2);
+dg_lt_beam.geom.Q               =                [0, 0, -2700];
+dg_lt_beam.geom.P               =                    [0, 12.5];
+dg_lt_beam.mat                  =                          mat;
+
+data.DG_Laengstraeger           =                   dg_lt_beam;
+
+# Dach (Schale)
+
+dg_surface.type                 =                   "elements";
+dg_surface.name                 =                         "s4";
+dg_surface.geom.t               =                            dg_surface_geom_t;
+dg_surface.mat                  =                          mat;
+
+data.DG_Dach                    =                   dg_surface;
+   
+#Randbedingungen
+data.Auflager = struct("type", "constraints",
+                        "name", "prescribed",
+                        "dofs", 3);
+
+data.Drehzapfen =  struct("type", "constraints",
+                        "name", "prescribed",
+                        "dofs", 1:2);                       
+
+
+model = mfs_import(fid,waggon_msh_file, "msh", data);
+waggon = mfs_new(fid, model);
+
+#Ausgabe Achsen
+mfs_export("waggon.axes", "msh", waggon, "mesh", "axes");
+
+#Matrizen berechnen
+
+waggon                          =            mfs_stiff(waggon);
+waggon                          =             mfs_mass(waggon);
+mass_prop = mfs_massproperties(fid, waggon);
+
+#Eigenschwingungen
+
+waggon                          =  mfs_freevib(waggon, nofmod);
+
+lowest_energy_mode = waggon.modes.freq(1);
+
+result = [lowest_energy_mode mass_prop.m];
+
+#Berechnung
+#mfs_print(fid, waggon, "modes", "freq");
+#mfs_export(["aufgabe2", ".dsp"], "msh", waggon, "modes", "disp");
+
+fclose(fid);
+
+#save("-binary", ["aufgabe2", ".dat"], "data" );
+
+endfunction
